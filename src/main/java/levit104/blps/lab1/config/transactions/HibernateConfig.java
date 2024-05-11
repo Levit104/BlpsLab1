8fa09ed7a4a888a8ae69ce22e175c91f5ef157a2
Artyom <artyom0104@gmail.com>
2024-05-11 15:00:59 +0300

    Move Hibernate config to application.yml

--- src/main/java/levit104/blps/lab1/config/transactions/HibernateConfig.java
+++ src/main/java/levit104/blps/lab1/config/transactions/HibernateConfig.java
@@ -1,23 +1,28 @@
 package levit104.blps.lab1.config.transactions;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.util.Map;
 
 @Configuration
+@RequiredArgsConstructor
 public class HibernateConfig {
+    private final Environment env;
+
     @Bean
     public Map<String, String> jpaProperties() {
         return Map.of(
                 // https://www.atomikos.com/Documentation/HibernateIntegration
-                "hibernate.connection.handling_mode", "DELAYED_ACQUISITION_AND_RELEASE_AFTER_STATEMENT",
+                "hibernate.connection.handling_mode", env.getRequiredProperty("hibernate.connection.handling_mode"),
 
-                "hibernate.hbm2ddl.auto", "update",
-                "hibernate.show_sql", "true",
-                "hibernate.format_sql", "false",
-                "hibernate.physical_naming_strategy", "org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy",
-                "hibernate.implicit_naming_strategy", "org.springframework.boot.orm.jpa.hibernate.SpringImplicitNamingStrategy"
+                "hibernate.hbm2ddl.auto",  env.getRequiredProperty("hibernate.hbm2ddl.auto"),
+                "hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"),
+                "hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"),
+                "hibernate.physical_naming_strategy", env.getRequiredProperty("hibernate.physical_naming_strategy"),
+                "hibernate.implicit_naming_strategy", env.getRequiredProperty("hibernate.implicit_naming_strategy")
         );
     }
 }
