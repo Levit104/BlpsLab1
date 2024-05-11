99aa19b56552c85be03508276e2a971ca7805bba
Artyom <artyom0104@gmail.com>
2024-05-11 20:10:45 +0300

    Enabled CSRF

--- src/main/java/levit104/blps/lab1/config/SecurityConfig.java
+++ src/main/java/levit104/blps/lab1/config/SecurityConfig.java
@@ -6,10 +6,10 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -19,13 +19,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic(Customizer.withDefaults())
-                /*
                 .csrf(configurer -> configurer
                         .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
                         .ignoringRequestMatchers("/auth/registration")
                 )
-                 */
-                .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(AbstractHttpConfigurer::disable)
         ;
 
         return http.build();
--- src/main/java/levit104/blps/lab1/config/transactions/HibernateConfig.java
+++ src/main/java/levit104/blps/lab1/config/transactions/HibernateConfig.java
@@ -18,7 +18,7 @@ public class HibernateConfig {
                 // https://www.atomikos.com/Documentation/HibernateIntegration
                 "hibernate.connection.handling_mode", env.getRequiredProperty("hibernate.connection.handling_mode"),
 
-                "hibernate.hbm2ddl.auto",  env.getRequiredProperty("hibernate.hbm2ddl.auto"),
+                "hibernate.hbm2ddl.auto", env.getRequiredProperty("hibernate.hbm2ddl.auto"),
                 "hibernate.show_sql", env.getRequiredProperty("hibernate.show_sql"),
                 "hibernate.format_sql", env.getRequiredProperty("hibernate.format_sql"),
                 "hibernate.physical_naming_strategy", env.getRequiredProperty("hibernate.physical_naming_strategy"),
